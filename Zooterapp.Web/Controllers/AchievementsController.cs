f2ee0d90dec8e662dbceb1b7356b207a0ade9695
BryanMazo <bryanmazo209296@correo.itm.edu.co>
2019-08-17 14:23:12 -0500

    Workin remote

--- Zooterapp.Web/Controllers/AchievementsController.cs
+++ Zooterapp.Web/Controllers/AchievementsController.cs
@@ -19,12 +19,24 @@ namespace Zooterapp.Web.Controllers
         }
 
         // GET: Achievements
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(await _context.Achievements.ToListAsync());
+            return View(_context.Achievements
+                .Include(a => a.PetAchievements)
+                .ThenInclude(pa => pa.Pet)
+                .ThenInclude(p => p.Name)
+                .Include(a => a.PetAchievements)
+                .ThenInclude(pa => pa.Pet)
+                .ThenInclude(p => p.Age)
+                .Include(a => a.PetAchievements)
+                .ThenInclude(pa => pa.Pet)
+                .ThenInclude(p => p.Race)
+                .Include(a => a.PetAchievements.Count)
+                .Include(a => a.PetAchievements)
+                .ThenInclude(pa => pa.Pet)
+                .ThenInclude(p => p.Owner));
         }
 
-        // GET: Achievements/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -33,6 +45,19 @@ namespace Zooterapp.Web.Controllers
             }
 
             var achievement = await _context.Achievements
+                .Include(a => a.PetAchievements)
+                .ThenInclude(pa => pa.Pet)
+                .ThenInclude(p => p.Name)
+                .Include(a => a.PetAchievements)
+                .ThenInclude(pa => pa.Pet)
+                .ThenInclude(p => p.Age)
+                .Include(a => a.PetAchievements)
+                .ThenInclude(pa => pa.Pet)
+                .ThenInclude(p => p.Race)
+                .Include(a => a.PetAchievements.Count)
+                .Include(a => a.PetAchievements)
+                .ThenInclude(pa => pa.Pet)
+                .ThenInclude(p => p.Owner)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (achievement == null)
             {
@@ -40,113 +65,6 @@ namespace Zooterapp.Web.Controllers
             }
 
             return View(achievement);
-        }
-
-        // GET: Achievements/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Achievements/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Achievement achievement)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(achievement);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(achievement);
-        }
-
-        // GET: Achievements/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var achievement = await _context.Achievements.FindAsync(id);
-            if (achievement == null)
-            {
-                return NotFound();
-            }
-            return View(achievement);
-        }
-
-        // POST: Achievements/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Achievement achievement)
-        {
-            if (id != achievement.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(achievement);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AchievementExists(achievement.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(achievement);
-        }
-
-        // GET: Achievements/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var achievement = await _context.Achievements
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (achievement == null)
-            {
-                return NotFound();
-            }
-
-            return View(achievement);
-        }
-
-        // POST: Achievements/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var achievement = await _context.Achievements.FindAsync(id);
-            _context.Achievements.Remove(achievement);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool AchievementExists(int id)
-        {
-            return _context.Achievements.Any(e => e.Id == id);
         }
     }
 }

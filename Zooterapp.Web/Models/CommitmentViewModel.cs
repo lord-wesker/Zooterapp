1f8effa92c49942958c70d41a6d9a1f6728957a2
Yeison Calle <yeisoncalle225710@correo.itm.edu.co>
2019-08-17 22:21:22 -0500

    Edit Pet, Add pet Images, Add Commitment

---
+++ Zooterapp.Web/Models/CommitmentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Zooterapp.Web.Data.Entities;
+
+namespace Zooterapp.Web.Models
+{
+    public class CommitmentViewModel : Commitment
+    {
+        public int PetOwnerId { get; set; }
+
+        public int PetId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Customer")]
+        [Range(1, int.MaxValue, ErrorMessage = "You must select a Customer.")]
+        public int CustomerId { get; set; }
+
+        public IEnumerable<SelectListItem> Customers { get; set; }
+
+    }
+}

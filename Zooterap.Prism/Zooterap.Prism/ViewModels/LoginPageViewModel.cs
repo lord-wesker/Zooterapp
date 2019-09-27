03a8d22806e06dfc3648084502482a8d557df159
BryanMazo <bryanmazo209296@correo.itm.edu.co>
2019-09-27 09:32:39 -0500

    problem with user data

--- Zooterap.Prism/Zooterap.Prism/ViewModels/LoginPageViewModel.cs
+++ Zooterap.Prism/Zooterap.Prism/ViewModels/LoginPageViewModel.cs
@@ -66,9 +66,6 @@ namespace Zooterap.Prism.ViewModels
                 return;
             }
 
-            IsRunning = true;
-            IsEnabled = false;
-
             var url = App.Current.Resources["UrlAPI"].ToString();
             var connection = await _apiService.CheckConnectionAsync(url);
             if (!connection)
@@ -98,7 +95,32 @@ namespace Zooterap.Prism.ViewModels
             }
 
             var token = response.Result;
-            await _navigationService.NavigateAsync("PetsPage");
+            var response2 = await _apiService.GetPetOwnerByEmailAsync(
+                url,
+                "api",
+                "/PetOwners/GetPetOwnerByEmail",
+                "bearer",
+                token.Token,
+                Email);
+
+            if (!response2.IsSuccess)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await App.Current.MainPage.DisplayAlert("Error", "Problem with user data, call 1-800-9238.", "Accept");
+                return;
+            }
+
+            var petowner = response2.Result;
+            var parameters = new NavigationParameters
+            {
+                { "petowner", petowner }
+            };
+
+            await _navigationService.NavigateAsync("PetsPage", parameters);
+
+            IsEnabled = true;
+            IsRunning = false;
 
         }
 
--- Zooterap.Prism/Zooterap.Prism/ViewModels/PetsPageViewModel.cs
+++ Zooterap.Prism/Zooterap.Prism/ViewModels/PetsPageViewModel.cs
@@ -1,15 +1,29 @@
 ﻿using Prism.Mvvm;
 using Prism.Navigation;
+using Zooterapp.Common.Models;
 
 namespace Zooterap.Prism.ViewModels
 {
     public class PetsPageViewModel : ViewModelBase
     {
+        private PetOwnerResponse _petowner;
         private readonly INavigationService _navigationService;
         public PetsPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             _navigationService = navigationService;
             Title = "Pets";
         }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            if (parameters.ContainsKey("petowner"))
+            {
+                _petowner = parameters.GetValue<PetOwnerResponse>("petowner");
+                Title = _petowner.FullName;
+            }
+        }
+
     }
 }

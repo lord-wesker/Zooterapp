40d344644d2dd6ec8cfa65d2d7c01e66d182b1c6
BryanMazo <bryanmazo209296@correo.itm.edu.co>
2019-10-19 14:09:00 -0500

    Password Recovery

--- Zooterapp.Web/Helpers/IUserHelper.cs
+++ Zooterapp.Web/Helpers/IUserHelper.cs
@@ -37,5 +37,9 @@ namespace Zooterapp.Web.Helpers
 
         Task<User> GetUserByIdAsync(string userId);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
     }
 }
